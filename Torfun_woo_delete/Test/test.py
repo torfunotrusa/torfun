fdaecb1b20326e5678f84ae9f2241ba2ef98d562
torfunotrusa <torfun@otrusa.com>
2022-12-28 17:02:57 -0500

    02

--- Torfun_woo_delete/Test/test.py
+++ Torfun_woo_delete/Test/test.py
@@ -1,18 +1,22 @@
+import json
+from woocommerce import API
 
+wcapi = API(
+    url="https://www.otrusa.com/",
+    consumer_key="ck_345a09819d5dc2ad29f36fc23088076cacdbc085",
+    consumer_secret="cs_440a066d02641852054093c9dd70f2cbc1b92436",
+    version="wc/v3"
+)
 
-import pandas as pd
+deleted_id = [707380,843232,843295,843330,843333,843608,843863,843864,843873,843932,843933,844144,844450,844660,844711,844999,845067,845102,845107,845182,845258,845336,845350,845407,845466,845609,845863,845879,846271,846296,846525,846540,846545,846731,846891,846942,847048,847060,847101,847105,847131,847258,847181,847299,847337,847485,847493,847601,847643,847646,847954,848016]
 
-a = 5
-b = 10
-c = 15
+for i in deleted_id:
+    # print (i)
+   x = (wcapi.get(f"products/{i}").json())
+   y = x["name"]
+   print(y)
 
-list = [5,10,15]
+# product_data = wcapi.delete("products/652084").json()
+# product_deleted_name = product_data["name"]
 
-result = (a+b+c)/3
-
-print (result)
-
-result02 = sum(list)
-print (result02)
-
-
+# print(f"this product has been deleted > {product_deleted_name}")
