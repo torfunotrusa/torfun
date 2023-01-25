98908c0e0d2d049f5dad1047c87eed309bc48996
torfunotrusa <torfun@otrusa.com>
2023-01-25 17:27:22 -0500

    Deleted duplicate items

--- Torfun_odoo/Torfun_odoo.py
+++ Torfun_odoo/Torfun_odoo.py
@@ -1,15 +1,36 @@
 import odoolib
-print(odoo_records)
+
 
 connection = odoolib.get_connection(hostname ="otrusa.odoo.com",
     database = "otrusa-otrusa-331982",
     login = "torfun@otrusa.com",
     password ="Otrusa@11201",
     port = 443,
-    odoo_protocol = "jsonrpcs"
+    protocol = "jsonrpcs"
 )
 
 odoo_model_data = connection.get_model('product.template')
 
-odoo_records = odoo_model_data.search_read([('id', '=',41452)], {'fields':['id','name','list_price']})
+# odoo_records = odoo_model_data.search_read(['&',('create_uid', '=',65),('list_price','>',1200)], ['id','name','list_price','create_uid'])
+
+# Find Average List Price
+
+odoo_records = odoo_model_data.search_read(['&',('create_uid', '=',65),('list_price','>',1200)], ['id','name','list_price'])
+print(odoo_records)
+
+list_price = []
+
+for i in odoo_records:
+    value = i.get('list_price')
+    list_price.append(value)
+
+
+print(list_price)
+
+total = sum(list_price)
+length = len(list_price)
+average = total/length
+print(average)
+
+
 
